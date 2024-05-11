91418e3d678c254e3476b4b4f1d0a30e07261bfe
ManuelGurbanov <manuelgurbanov@gmail.com>
2024-05-11 00:04:35 -0300

    Added android sidemenu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -25,9 +25,13 @@ export default function Nav() {
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
             onClick={() => setMobileMenuOpen(true)}
           >
-            <span className="sr-only">Open main menu</span>
+          <h1
+                className="text-xl font-bold text-green-500">Menú
+           </h1>
           </button>
         </div>
+
+
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
             <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white hover:text-green-500">
@@ -35,21 +39,26 @@ export default function Nav() {
             </a>
           ))}
         </div>
+
+
       </nav>
+
+
       <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-gray-900 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-                <Coffe />
-            </a>
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-zinc-900 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+          <div className="flex items-center justify-start">
+
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-400"
               onClick={() => setMobileMenuOpen(false)}
             >
-              <span className="sr-only">Close menu</span>
+
+            <h1
+                  className="text-xl font-bold text-green-500">Cerrar
+            </h1>
+
             </button>
           </div>
           <div className="flow-root mt-6">
@@ -69,6 +78,8 @@ export default function Nav() {
           </div>
         </Dialog.Panel>
       </Dialog>
+
+      
     </header>
   )
 }
